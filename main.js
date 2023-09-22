19f8512f9d3b282b8bf070c4fdd18067dd8b8165
ESTELA <estelafollari@gmail.com>
2023-09-22 17:58:00 -0300

    final 2.0

--- main.js
+++ main.js
@@ -2,19 +2,21 @@ const container = document.getElementById("container");
 const botonNext = document.getElementById("boton-next");
 const botonPrev = document.getElementById("boton-prev");
 const filtroGenero = document.getElementById("filtroGenero");
+const botonPrimera = document.getElementById("boton-primera");
+const botonUltima = document.getElementById("boton-ultima");
 
 //console.log(filtroGenero.value);
 //console.log(container);
 let paginacion = 1;
 let totalPaginas = 1;
-let filtro="";
+let filtro = "";
 //let valorfiltro="unknown"
 //let todo = "female&gender=male&gender=genderless&gender=unknown";
 
-const getPersonajes = (paramFiltro,pag) => {
-   // console.log(paramFiltro,pag);
+const getPersonajes = (paramFiltro, pag) => {
+    // console.log(paramFiltro,pag);
     container.innerHTML = ""; // borra el contenedor
-    
+
     fetch(`https://rickandmortyapi.com/api/character/?page=${pag}${paramFiltro}`)
 
         .then(res => res.json())
@@ -24,10 +26,11 @@ const getPersonajes = (paramFiltro,pag) => {
             //console.log("total paginas en fetch",totalPaginas)
         })
 
-        
+
 }
+botonPrimera.setAttribute("disabled", true);
 botonPrev.setAttribute("disabled", true);
-getPersonajes(filtro,paginacion);
+getPersonajes(filtro, paginacion);
 
 
 
@@ -36,25 +39,26 @@ const renderPersonajes = (data) => {
     //console.log("total paginas en render",totalPaginas)
     if (totalPaginas == 1) {
         //console.log ("totalPaginas==1")
-        botonNext.setAttribute("disabled", false);
-     }
-     //else { botonNext.setAttribute("enabled", true); }         
-   
-     data.results.forEach(personaje => {
+        botonNext.disabled = true;
+        botonUltima.disabled = true;
+        botonPrimera.disabled = true;
+        botonPrimera.disabled = true;
+    }
+    data.results.forEach(personaje => {
         container.innerHTML +=
-        `<div class="card">
-            <div class="frente">
-                <h2>${personaje.name}</h2>
-                <img src="${personaje.image}" alt="">
-            </div>
-            <div class="atras">
-                <h1>${personaje.name}</h1>
-                <p><strong>Origen:</strong> ${personaje.origin.name}</p>
-                <p><strong>Especie:</strong> ${personaje.species}</p>
-                <p><strong>Estado:</strong> ${personaje.status}</p>
-                <p><strong>Género:</strong> ${personaje.gender}</p>
-            </div>
-        <div>`
+            `<div class="card">
+                <div class="frente">
+                    <h2>${personaje.name}</h2>
+                    <img src="${personaje.image}" alt="">
+                </div>
+                <div class="atras">
+                    <h1>${personaje.name}</h1>
+                    <p><strong>Origen:</strong> ${personaje.origin.name}</p>
+                    <p><strong>Especie:</strong> ${personaje.species}</p>
+                    <p><strong>Estado:</strong> ${personaje.status}</p>
+                    <p><strong>Género:</strong> ${personaje.gender}</p>
+                </div>
+            <div>`
     });
 }
 
@@ -80,43 +84,85 @@ botonPrev.addEventListener("click", () => {
         botonNext.removeAttribute("disabled", false);
         paginacion--;
         botonPrev.removeAttribute("disabled", false);
+        botonPrimera.disabled = false;
+        botonUltima.disabled = false;
     }
 
     if (paginacion <= 1) {
         botonPrev.setAttribute("disabled", true);
+        botonPrimera.disabled = true;
     }
 
     //console.log(`llamo desde PREV a getPersonajes con ${paginacion}`)
-    getPersonajes(filtro,paginacion);
+    getPersonajes(filtro, paginacion);
 });
 
 botonNext.addEventListener("click", () => {
-    //console.log(paginacion)
-    if (paginacion <= 1) {
-        botonPrev.removeAttribute("disabled", false);
-        if (totalPaginas>1) paginacion++;
-        else botonNext.setAttribute("disabled", false);
-    } else {
-        if (paginacion > 1 && paginacion < totalPaginas) {
-            //botonNext.setAttribute("disabled", false);
-            paginacion++;
-            //console.log("entra", paginacion)
+   // console.log(paginacion)
+    if (paginacion == 1) {  //si estoy en la pag 1
+        if (totalPaginas == 1){  // si solo hay una pagina
+            botonNext.disabled = true;
+            botonUltima.disabled = true;
+            botonPrev.disabled = true;
+            botonPrimera.disabled = true;
         }
-    }
+        else{ // si hay mas de una pagina
+        botonPrev.disabled = false;
+        botonPrimera.disabled = false;
+        botonNext.disabled = false;
+        botonUltima.disabled = false;
+        paginacion++;
+        }
+       
+    } else {
+        if (paginacion > 1 && paginacion < totalPaginas)  paginacion++;
+        }
     if (paginacion == totalPaginas) {
-        botonNext.setAttribute("disabled", false);
-       // console.log("entra ultimo else",paginacion)
+        botonNext.disabled = true;
+        botonUltima.disabled = true;
+        // console.log("entra ultimo else",paginacion)
     }
     //console.log(`llamo desde NEXT a getPersonajes con ${paginacion}`)
-    getPersonajes(filtro,paginacion);
+    getPersonajes(filtro, paginacion);
+});
+
+botonPrimera.addEventListener("click", () => {
+    botonPrimera.disabled = true;
+    botonPrev.disabled = true;
+    if (totalPaginas == 1){
+        botonNext.disabled = true;
+        botonUltima.disabled = true;
+    }
+    else {
+        botonNext.disabled = false;
+        botonUltima.disabled = false;
+    }
+    getPersonajes(filtro, paginacion = 1);
+
+});
+
+botonUltima.addEventListener("click", () => {
+    paginacion = totalPaginas;
+    if (totalPaginas == 1){
+        botonPrimera.disabled = true;
+        botonNext.disabled = true;
+    }
+    else{
+        botonPrimera.disabled = false;
+        botonPrev.disabled = false;
+    }
+    botonNext.disabled = true;
+    botonUltima.disabled = true;
+    getPersonajes(filtro, paginacion);
 });
 
 filtroGenero.addEventListener("change", () => {
-   // console.log(filtroGenero);
+    // console.log(filtroGenero);
     if (filtroGenero.value == "todos") filtro = "";
     else filtro = `&gender=${filtroGenero.value}`;
-    
+    botonUltima.disabled = false;
     botonNext.disabled = false;
     botonPrev.setAttribute("disabled", true);
-    getPersonajes(filtro,paginacion=1);
+    botonPrimera.disabled = true;
+    getPersonajes(filtro, paginacion = 1);
 });
